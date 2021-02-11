8c14633c59ac269ad297646ff9c092e135a27416
Carriel-JT <78246105+Carriel-JT@users.noreply.github.com>
2021-02-11 07:21:07 -0300

    iniciando

---
+++ forca.py
@@ -0,0 +1,11 @@
+def jogar():
+
+    print("*********************************")
+    print("***Bem vindo ao Jogo de Forca!***")
+    print("*********************************")
+
+
+
+    print("fim do jogo")
+
+jogar()
